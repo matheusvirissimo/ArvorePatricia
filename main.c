33a36efec2491d0d2517eb1212687355e8c48949
matheusvirissimo <matheus.virissimo@unesp.br>
2024-09-16 17:32:39 -0300

    feat: verificando o bit

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 #include <stdbool.h>
+
+#define bits_na_chave 32 // quantidade de bits disponíveis por chave
 
 typedef struct noPatricia{
     int bit; // posição do bit que discrimina o nó, isso é, para qual lado ele vai seguir 
@@ -17,3 +20,19 @@ void inicializaArvore(NOPATRICIA **arvore){
     (*arvore)->bit = -1;
 }
 
+// Função para retornar o valor 0 ou 1 da chave sem considerar sinal (unsigned) do bit na posição k
+unsigned bit(unsigned chave, int k){
+    // chave >> é uma operação de deslocamento de bit à direta, que move até a posição k
+    return chave >> (bits_na_chave - 1 - k) & 1;
+    // faz uma operação de AND para saber qual é o bit.
+}
+
+// Inserção - inserir uma nova chave na árvore
+
+
+// Remoção - Remover uma chave existente da árvore
+
+// Busca - Verificar se uma chave está presente
+
+
+// Impressão - imprimir a árvore exibindo as chaves e a discriminação por bits
